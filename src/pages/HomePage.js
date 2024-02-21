a9705823db53b2b1d33eef92cd0d317e684f716b
timthebouncer <he01367637us@gmail.com>
2024-02-21 23:24:43 +0800

    資料換字 / 聯絡圖示 / 影片改shorts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import InstagramOutlined from "@ant-design/icons/lib/icons/InstagramOutlined";
 import FacebookOutlined from "@ant-design/icons/lib/icons/FacebookOutlined";
 import YoutubeOutlined from "@ant-design/icons/lib/icons/YoutubeOutlined";
@@ -7,22 +7,21 @@ import {videoList} from "../data";
 import style from './HomePage.css'
 
 const HomePage=()=>{
+    const [isHideText, setIsHideText] = useState(true)
     return (
     <div>
         <header className="header-wrapper">
-            <h1>BBall_Vision</h1>
-            <div className="sub-title">
-                收錄精華以及您不知道的NBA大小事
+            <div>
+                <MenuFoldOutlined />
             </div>
-            <h4 className='sub-title'>籃球傳教士 | NBA消息 | 球員報導</h4>
-            {/*<div>*/}
-            {/*    <MenuFoldOutlined />*/}
-            {/*</div>*/}
-            {/*<div>*/}
-            {/*    <InstagramOutlined />*/}
-            {/*    <FacebookOutlined />*/}
-            {/*    <YoutubeOutlined />*/}
-            {/*</div>*/}
+            <div>
+                <h1>BBall_Vision</h1>
+                <div className="sub-title">
+                    收錄精華以及您不知道的NBA大小事
+                </div>
+                <h4 className='sub-title'>籃球傳教士 | NBA消息 | 球員報導</h4>
+            </div>
+
             {/*<nav>*/}
             {/*    <a href=""></a>*/}
             {/*    <a href=""></a>*/}
@@ -39,13 +38,23 @@ const HomePage=()=>{
                                 <iframe
                                     id="inlineFrameExample"
                                     title="Inline Frame Example"
-                                    width="100%"
-                                    height="200"
+                                    width="99%"
+                                    height="500"
                                     src={item.videoUrl}l>
                                 </iframe>
-                                <div className='intro-text'>
+                                <div className={isHideText && item.intro.length > 75 ? 'intro-text hide-text':'intro-text'}>
                                     {item.intro}
+
                                 </div>
+                                {
+                                    item.intro.length > 75 && <div className='seeMoreBtn-wrapper'>
+                                        <button
+                                            className='seeMoreBtn'
+                                            onClick={()=>setIsHideText(!isHideText)}>{isHideText ? '查看更多':'查看更少'}</button>
+                                    </div>
+                                }
+
+
                             </div>
                         )
                     })
@@ -59,6 +68,11 @@ const HomePage=()=>{
                     <a href="">廣告合作</a>
                     <a href="">服務條款及隱私權政策</a>
                     <a href="">聯絡我們</a>
+                    <div>
+                        <InstagramOutlined />
+                        <FacebookOutlined />
+                        <YoutubeOutlined />
+                    </div>
                 </div>
             </div>
             <div className='copyright'>
